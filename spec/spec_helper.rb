8729bd58196231885d89385b38c97431fc1f9ac9
Bill Dueber <bill@dueber.com>
2011-03-13 23:31:48 -0400

    Added stuff for embedded solr server testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,14 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
-require 'SolrQuery'
+require 'solrquery'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
+
+## Define the solr embedded system
+
 RSpec.configure do |config|
-  
 end
