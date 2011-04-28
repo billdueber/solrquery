346b48e7ac62a7ca21bac3e6f6f48404c68e6be8
BillDueber <bill@dueber.com>
2011-04-28 16:59:57 -0400

    Changed specs to use new initialize syntax

--- spec/AndOrNot_spec.rb
+++ spec/AndOrNot_spec.rb
@@ -1,6 +1,6 @@
 describe "A negated lucene query" do
   it "does a simple query" do
-    q = SolrQuery::Lucene.new 'solr', 'title'
+    q = SolrQuery::Lucene.new :search=>'solr', :fields=>{'title' => 1}
     (-q).query['q'].should match /\(NOT _query_:"\{!lucene df='title' v=\$q(\d+)\}"\)/
   end
 end
@@ -9,14 +9,14 @@ end
 
 describe "A compound lucene query" do
   before(:each) do
-    @q1 =   SolrQuery::Lucene.new 'solr', 'name', 3
-    @q2 =   SolrQuery::Lucene.new 'apache'
-    @q3 =   SolrQuery::Lucene.new 'three'
-    @q4 =   SolrQuery::Lucene.new 'four', 'name', 4
+    @q1 =   SolrQuery::Lucene.new :search=>'solr', :fields=>{'name' => 1}, :boost=>3
+    @q2 =   SolrQuery::Lucene.new :search=>'apache'
+    @q3 =   SolrQuery::Lucene.new :search=>'three'
+    @q4 =   SolrQuery::Lucene.new :search=>'four', :fields=>{'name' => 4}
   end
 
   it "can AND together two queries" do
-    query = (@q1 * @q2).query
+    query = (@q1 & @q2).query
     
     qmatch = /\(_query_:"{!lucene\s+df='name'\s+v=\$(q\d+)}"\^3\s+
                AND\s+
@@ -28,7 +28,7 @@ describe "A compound lucene query" do
   end
   
   it "can AND together two queries and use a boost" do
-    q = (@q1 * @q2)
+    q = (@q1 & @q2)
     q.boost = 10
     query = q.query
     qmatch = /\(_query_:"{!lucene\s+df='name'\s+v=\$(q\d+)}"\^3\s+
@@ -56,13 +56,13 @@ end
 
 describe "A compound dismax query" do
   before(:each) do
-    @q1 =   SolrQuery::DisMax.new 'kwone', {'one'=>10, 'oneone'=>11}
-    @q2 =   SolrQuery::DisMax.new 'kwtwo', {'twp' => 2}
-    @q3 =   SolrQuery::DisMax.new 'kwthree', {'three'=>3}
-    @q4 =   SolrQuery::DisMax.new 'kwfour', {'four'=>4}, {'pffour'=>444}
+    @q1 =   SolrQuery::DisMax.new :search=>'kwone', :fields=>{'one'=>10, 'oneone'=>11}
+    @q2 =   SolrQuery::DisMax.new :search=>'kwtwo', :fields=>{'twp' => 2}
+    @q3 =   SolrQuery::DisMax.new :search=>'kwthree', :fields=>{'three'=>3}
+    @q4 =   SolrQuery::DisMax.new :search=>'kwfour', :fields=>{'four'=>4, 'pffour'=>444}
   end
 
   it "should work" do
-    pp (@q1 | (@q3 * @q4)).query
+    pp (@q1 | (@q3 & @q4)).query
   end
 end
--- spec/basics_spec.rb
+++ spec/basics_spec.rb
@@ -4,7 +4,7 @@ require 'pp'
 describe "Interal ops" do
   describe "A simple Lucene query" do
     it "with one term" do
-      q = SolrQuery::Lucene.new "solr"
+      q = SolrQuery::Lucene.new :search=>"solr"
       query = q.query
       qmatch = /_query_:"\{!lucene v=\$(q\d+)\}"/
       query['q'].should match qmatch
@@ -13,7 +13,7 @@ describe "Interal ops" do
     end
     
     it "with multiple terms" do
-      q = SolrQuery::Lucene.new "solr apache"
+      q = SolrQuery::Lucene.new :search=>"solr apache"
       query = q.query
       qmatch = /_query_:"\{!lucene v=\$(q\d+)\}"/
       query['q'].should match qmatch
@@ -23,23 +23,23 @@ describe "Interal ops" do
     
   
     it "with a boost" do
-      q = SolrQuery::Lucene.new "solr apache"
+      q = SolrQuery::Lucene.new :search=>"solr apache"
       q.boost = 3
       q.query['q'].should match /_query_:"\{!lucene v=\$q(\d+)\}"\^3/
     end
       
     it "with a field" do
-      q = SolrQuery::Lucene.new 'solr', 'title'
+      q = SolrQuery::Lucene.new :search=>'solr', :fields=>{'title' => 1}
       q.query['q'].should match /_query_:"\{!lucene df='title' v=\$q(\d+)\}"/
     end
       
     it "with all three" do
-      q = SolrQuery::Lucene.new %w(solr apache), 'title', 3
+      q = SolrQuery::Lucene.new :search=>%w(solr apache), :field=>'title', :boost=>3
       q.query['q'].should match /_query_:"{!lucene df='title' v=\$q(\d+)}"\^3/
     end
       
     it "uses default boolean operator" do
-      q = SolrQuery::Lucene.new %w(solr apache), 'title', 3
+      q = SolrQuery::Lucene.new :search=>%w(solr apache), :field=>'title', :boost=>3
       q.defaultOp = 'AND'
       q.query['q'].should match /_query_:"\{!lucene q.op='AND' df='title'\s+v=\$q(\d+)\}"\^3/
       q.defaultOp = 'OR'
@@ -47,18 +47,18 @@ describe "Interal ops" do
     end
       
     it "rejects a non-AND/OR boolean" do
-      q = SolrQuery::Lucene.new %w(solr apache), 'title', 3
+      q = SolrQuery::Lucene.new :search=>%w(solr apache), :fields=>{'title' => 3}
       lambda {q.defaultOp = 'NOT'}.should raise_error
     end
       
     it "uses a default field" do
-      q = SolrQuery::Lucene.new %w(solr apache), 'title', 3
-      q.field = 'all'
+      q = SolrQuery::Lucene.new :search=>%w(solr apache), :boost=> 3
+      q.defaultField = 'all'
       q.query['q'].should match /_query_:"\{!lucene df='all' v=\$q(\d+)\}"\^3/
     end
       
     it "can deal with a null field and positive boost" do
-      q = SolrQuery::Lucene.new %w(solr apache), nil, 3
+      q = SolrQuery::Lucene.new :search=>%w(solr apache), :boost=>3
       q.query['q'].should match /_query_:"\{!lucene v=\$q(\d+)\}"\^3/
     end
   end
